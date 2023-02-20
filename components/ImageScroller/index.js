12aa6de38de5a88a6cd9fca99b3bfbac014e8ec7
devRabbani <dev.golamrabbani@gmail.com>
2023-02-20 20:58:02 +0530

    📦 NEW: Added Image modal

--- components/ImageScroller/index.js
+++ components/ImageScroller/index.js
@@ -1,22 +1,33 @@
 import Image from 'next/image'
-import { RiCloseFill } from 'react-icons/ri'
+import { useState } from 'react'
+import { RiCloseFill, RiZoomInLine } from 'react-icons/ri'
+import Modal from '../Modal'
 import s from './imageScroller.module.css'
 
 export default function ImageScroller({ images, removeImage, edit }) {
-  return (
-    <div className={s.imageScroller}>
-      {images.map((image, i) => (
-        <div className={s.img} key={i}>
-          <Image src={image.img} alt="addad" fill />
-          {edit ? (
-            <span onClick={() => removeImage(i)} className={s.delete}>
-              <RiCloseFill />
-            </span>
-          ) : null}
+  const [modalImg, setModalImg] = useState(null)
 
-          <span className={s.option}>{edit ? i + 1 : image.option}</span>
-        </div>
-      ))}
-    </div>
+  return (
+    <>
+      <div className={s.imageScroller}>
+        {images.map((image, i) => (
+          <div className={s.img} key={i}>
+            <Image src={image.img} alt="addad" fill />
+            {edit ? (
+              <span onClick={() => removeImage(i)} className={s.delete}>
+                <RiCloseFill />
+              </span>
+            ) : (
+              <span className={s.zoom} onClick={() => setModalImg(image.img)}>
+                <RiZoomInLine />
+              </span>
+            )}
+
+            <span className={s.option}>{edit ? i + 1 : image.option}</span>
+          </div>
+        ))}
+      </div>
+      <Modal img={modalImg} setModalImg={setModalImg} />
+    </>
   )
 }
