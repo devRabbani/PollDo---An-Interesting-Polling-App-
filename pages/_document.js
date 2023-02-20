632950abf38b400b01013df108a1c7c3e6f3f49b
devRabbani <dev.golamrabbani@gmail.com>
2023-02-21 00:12:38 +0530

    📦 NEW: Added meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -7,8 +7,35 @@ export default function Document() {
         <meta name="theme-color" content="#32899e" />
         <meta
           name="description"
-          content="It is an ineteresting polling or voting app"
+          content="Create and share your opinions and polls with images on this app. Choose your privacy settings and share your polls with friends."
         />
+        <meta
+          name="keywords"
+          content="opinions, polls, images, privacy, sharing"
+        />
+        <meta property="og:title" content="Opinion & Poll App" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.example.com/" />
+        <meta
+          property="og:image"
+          content="https://www.example.com/images/opinion-poll-app.jpg"
+        />
+        <meta
+          property="og:description"
+          content="Create and share your opinions and polls with images on this app. Choose your privacy settings and share your polls with friends."
+        />
+        <meta property="og:site_name" content="PollDoh" />
+        <meta name="twitter:title" content="PollDoh" />
+        <meta
+          name="twitter:description"
+          content="Create and share your opinions and polls with images on this app. Choose your privacy settings and share your polls with friends."
+        />
+        <meta
+          name="twitter:image"
+          content="https://polldoh.vercel.app/android-chrome-512x512.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+
         <link
           rel="apple-touch-icon"
           sizes="180x180"
